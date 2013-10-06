390980c4853b358f33cafb98c40c19ec3cd43c4e
KitchenSpoon <hljw4@hotmail.com>
2013-10-06 07:10:04 -0700

    Added templates(basically where the websites are) and modified the settings

--- feeds/models.py
+++ feeds/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Feeds(models.Model):
-	url=models.CharField(max_length=200)
-	dateAdded=models.DateTimeField()
+    url=models.CharField(max_length=200)
+    dateAdded=models.DateTimeField()
--- feeds/views.py
+++ feeds/views.py
@@ -1,1 +1,12 @@
+from django.template import Context,loader
+from django.http import HttpResponse
+#from feeds.models import Feeds
+
 # Create your views here.
+def index(request):
+    t=loader.get_template('feeds/index.html')
+    c=Context({
+        'lover':'Jack'
+    })	
+    return HttpResponse(t.render(c))
+

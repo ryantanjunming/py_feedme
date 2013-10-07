8393ac84f7737dc5b2760c3bb22cae678fa9e98c
Jackie Huynh <mizzinxsosleepy@gmail.com>
2013-10-08 10:17:32 +1100

    Made it work for more sites

--- feeds/views.py
+++ feeds/views.py
@@ -24,35 +24,49 @@ import time
 from datetime import datetime
 
 def feedTestRoar(request):
-    #sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='replace')
-    url = "http://www.theverge.com/rss/frontpage"
-    return HttpResponse(make_feed_page(feedparser.parse(url)))
-
-# PLAN: write a func to display a feed in its entirety (return HttpResponse)
-# includes writing something that constructs something to display each entry
-# main problem: entries don't come with images consistently
+    # so i can print out the debug msgs without exploding
+    sys.stdout = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='replace')
+    url = "http://feeds.feedburner.com/RockPaperShotgun"
+    #url = "http://www.theverge.com/rss/frontpage"
+    #url = "http://kotaku.com/vip.xml"
+    feed = feedparser.parse(url)
+    for k in feed.keys():
+        if k != "entries": print ">>", k, ":", feed[k]
+    for k in feed['feed'].keys():
+        print ">>", k, ":", feed['feed'][k]
+    return HttpResponse(make_feed_page(feed))
 
 def make_feed_page(feed):
     """
     Returns HTML (string) for displaying the feed object given.
     feed should be the returned value from feedparser.parse
     """
-    title_icon = "<img src=\"" + feed['feed']['icon'] + "\">"
-    title_header = feed['feed']['title'] + "<br>" + "<a href=\"" + feed['feed']['link'] + "\">" + title_icon + "</a>"
+    try:
+        img = feed['feed']['icon']
+    except (KeyError):
+        try:
+            img = feed['feed']['image']['href']
+        except (KeyError):
+            img = "IMG"
+    title_icon = "<img src=\"" + img + "\">"
+    title_header = "<h2><a href=\"" + feed['feed']['link'] + "\">" + feed['feed']['title'] + "</a></h2>" + \
+                   "<a href=\"" + feed['feed']['link'] + "\">" + title_icon + "</a>"
     
     updated_time = "Last Updated: "
     # note: I actually don't know if this is accurate... or how to grab timezone info
-    last_updated = datetime.fromtimestamp(time.mktime(feed['feed']['updated_parsed']))
+    try:
+        last_updated = datetime.fromtimestamp(time.mktime(feed['feed']['updated_parsed']))
+    except (KeyError):
+        last_updated = datetime.fromtimestamp(time.mktime(feed['updated_parsed']))
     updated_time += str(last_updated)
 
     feed_header = title_header + "<br>" + updated_time
 
     entries = ""
-
     for entry in feed['entries']:
         entries += "<hr>" + make_entry_string(entry)
             
-    page = feed_header + "<br>" + entries
+    page = feed_header + "<br>" + entries.decode('utf-8')
     return page
 
 def make_entry_string(entry):
@@ -63,12 +77,14 @@ def make_entry_string(entry):
     fields = {'link' : entry['link'],
               'title' : entry['title'],
               'author' : entry['author'],
-              'updated' : str(datetime.fromtimestamp(time.mktime(entry['updated_parsed']))),
+              'published' : str(datetime.fromtimestamp(time.mktime(entry['published_parsed']))),
               'summary' : entry['summary']}
+    for k in fields.keys():
+        fields[k] = fields[k].encode('utf-8')
 ##    entry = ("<h2>" + "<a href=\"" + entry['link'] + "\">" + entry['title'] + "</a></h2>" # linked heading
 ##             str(entry['author'])
 ##             ", updated " + str(datetime.fromtimestamp(time.mktime(entry['updated_parsed']))) + ""
 ##             "<br>" + entry['summary']
 ##             )
-    return "<h2><a href=\"{link}\">{title}</a></h2>{author}, updated at {updated}<br>{summary}".format(**fields)
+    return "<h3><a href=\"{link}\">{title}</a></h3>{author}, published on {published}<br>{summary}".format(**fields)
     

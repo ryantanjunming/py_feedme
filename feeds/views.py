5ceb9ceea31f39056fd1c0e3b79813c579f2303a
KitchenSpoon <hljw4@hotmail.com>
2013-10-12 02:03:28 -0700

    Finally made the buttons display its page on the right side of the page instead of opening a new page

--- feeds/views.py
+++ feeds/views.py
@@ -62,7 +62,8 @@ def insertFeed(request):
 def myFeeds(request):
     ret_str = ""
     for feed in selectAll():
-        ret_str += "<li><a href=\"" + "http://"+ request.META['HTTP_HOST'] + "/feeds/showfeed?url=" + feed.url + "\" target=\"_blank\">" + feed.name + "</a>"
+        #Jackie I've changed the one line below will it affect anything else? like the del_link_tag
+        ret_str += "<li><button type=\"button\" value=\"" + "http://"+ request.META['HTTP_HOST'] + "/feeds/showfeed?url=" + feed.url + "\" target=\"_blank\">" + feed.name + "</a>"
         # delete icon
         del_img = '<img src="{imgsrc}" alt="Delete Button" width="16" height="16">'
         del_img = del_img.format(imgsrc = os.path.join("..", "static", "img", "delete.png"))

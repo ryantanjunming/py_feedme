2f22227afbe8ba60b9171269c39238cb996e0915
KitchenSpoon <hljw4@hotmail.com>
2013-10-12 02:52:33 -0700

    Added new table/model Recommendations

--- feeds/models.py
+++ feeds/models.py
@@ -6,4 +6,10 @@ class Feeds(models.Model):
     url = models.CharField(max_length=200)
     dateAdded = models.DateTimeField()
 
+class Recommendations(models.Model):
+    url = models.CharField(max_length=200)
+    sender = models.CharField(max_length=200) #foreign key
+    receiver = models.CharField(max_length=200)#foreign key
+    #seen field unconfirmed, will implement when it comes to that.
+
 

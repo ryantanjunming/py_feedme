aaa30e801dc3b9e20ea613db694da5b563ce88f5
Ryan Tan <ryantanjunming@gmail.com>
2013-10-13 03:01:19 +1100

    changed URLs to have optional "/" at the tail end

--- feedme/urls.py
+++ feedme/urls.py
@@ -18,13 +18,13 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     
-    (r'^feeds/$','feeds.views.index'),
-    (r'^feeds/(?i)myFeeds$','feeds.views.myFeeds'),
-    (r'^feeds/insertFeed$','feeds.views.insertFeed'),
-    (r'^feeds/deleteFeed$','feeds.views.deleteFeed'),
-    (r'^feeds/insertRecommendation$','feeds.views.insertRecommendation'),
-    (r'^feeds/deleteRecommendation','feeds.views.deleteRecommendation'),
-    (r'^feeds/(?i)showfeed$', 'feeds.views.showFeed'),
-    (r'^feeds/(?i)billStripeToken$','feeds.views.billStripeToken'),
-    (r'^feeds/(?i)feederror$', TemplateView.as_view(template_name="feeds/feederror.html"))
+    (r'^feeds?/$','feeds.views.index'),
+    (r'^feeds/(?i)myFeeds?/$','feeds.views.myFeeds'),
+    (r'^feeds/insertFeed?/$','feeds.views.insertFeed'),
+    (r'^feeds/deleteFeed?/$','feeds.views.deleteFeed'),
+    (r'^feeds/insertRecommendation?/$','feeds.views.insertRecommendation'),
+    (r'^feeds/deleteRecommendation?/$','feeds.views.deleteRecommendation'),
+    (r'^feeds/(?i)showfeed?/$', 'feeds.views.showFeed'),
+    (r'^feeds/(?i)billStripeToken?/$','feeds.views.billStripeToken'),
+    (r'^feeds/(?i)feederror?/$', TemplateView.as_view(template_name="feeds/feederror.html"))
 )

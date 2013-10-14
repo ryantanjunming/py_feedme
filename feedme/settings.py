58bc0b5e84d55ee51ae73d9a738022fc16010bd7
Stanley Tan <me@stnly.com>
2013-10-14 13:27:00 +1100

    Add friendships

--- feedme/settings.py
+++ feedme/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'feeds',
     'accounts',
+    'friendship',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
